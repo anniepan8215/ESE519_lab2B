f149fe98539d40b74b0c912948a20b9b7b9ca532
anniepan8215 <xingqi@seas.upenn.edu>
2022-11-08 22:46:34 -0500

    modified

--- lab/02_repl/repl.c
+++ lab/02_repl/repl.c
@@ -3,34 +3,42 @@
 #include <string.h>
 #include "pico/stdlib.h"
 #include "pico/stdio_usb.h"
-#include "registers.h"
+// #include "registers.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 
 #define ADDRESS_LENGTH 8 //32 bits address
 #define MSG_LENGTH 8 //32 bits input
 
+uint32_t read_register(uint32_t *hex_addr){
+    return (uint32_t) *hex_addr;
+}
+
+void write_register(uint32_t *hex_addr, uint32_t hex_write){
+    *hex_addr = hex_write;
+}
+
 // initialize buffer
 char addr[8] = {'0'}; // store 8 bytes hex number = 32 bits binary number
 char msg[8] = {'0'}; // store 8 bytes hex number = 32 bits binary number
-ADDRESS hex_addr = 0; // addr convertor
-VALUE hex_msg = 0; // msg convertor
-VALUE value = 0; 
+uint32_t *hex_addr; // addr convertor
+uint32_t hex_msg = 0; // msg convertor
+uint32_t value = 0;
 int i_addr = 0; //iterator
 int i_msg = 0; //iterator
 
-uint16_t get_block(uint8_t *buffer) {
-  uint16_t buffer_index= 0;
-  while (true) {
-    int c = getchar_timeout_us(100);
-    if (c != PICO_ERROR_TIMEOUT && buffer_index < BUFFER_LENGTH) {
-      buffer[buffer_index++] = (c & 0xFF);
-    } else {
-      break;
-    }
-  }
-  return buffer_index;
-}
+// uint16_t get_block(uint8_t *buffer) {
+//   uint16_t buffer_index= 0;
+//   while (true) {
+//     int c = getchar_timeout_us(100);
+//     if (c != PICO_ERROR_TIMEOUT && buffer_index < BUFFER_LENGTH){
+//       buffer[buffer_index++] = (c & 0xFF);
+//     } else {
+//       break;
+//     }
+//   }
+//   return buffer_index;
+// }
 
 int main(){
     stdio_init_all(); //initialization
@@ -41,11 +49,14 @@ int main(){
         if(i_addr < ADDRESS_LENGTH){
             printf("Input register address:\n");
             // read from keyboard and store to buffer
-            // char c = getchar_timeout_us(1000);
-            // if(c!= PICO_ERROR_TIMEOUT){
-            //     addr[i_addr] = c;
-            //     i_addr++;
-            // }
+            char c = getchar_timeout_us(1000);
+            if(c!= PICO_ERROR_TIMEOUT){
+                addr[i_addr] = c;
+                i_addr++;
+            }else{
+                addr[i_addr] = 0;
+                i_addr++;
+            }
             
             continue;
         }
@@ -56,19 +67,26 @@ int main(){
             if(c!= PICO_ERROR_TIMEOUT){
                 msg[i_msg] = c;
                 i_msg++;
+            }else{
+                msg[i_msg] = 0;
+                i_msg++;
             }
             continue;
         }
         if(i_addr >= ADDRESS_LENGTH && i_msg >= MSG_LENGTH){
+            // char addr[] = "e0000000";
+            // char msg[] = "ffff0fff";
             printf("record address successfully %s\n",addr);
-            hex_addr = (ADDRESS)strtol(addr, NULL, 16);
+            *hex_addr = (int)strtol(addr, NULL, 16);
             printf("record message successfully %s\n",msg);
-            hex_msg = (VALUE)strtol(msg, NULL, 16);
+            hex_msg = (int)strtol(msg, NULL, 16);
             printf("Start write:\n");
-            register_write(hex_addr,hex_msg);
+            write_register(hex_addr,hex_msg);
             printf("Start read:\n");
-            value = register_read(hex_addr);
-            printf("The value read out is %d\n",value);
+            value = read_register(hex_addr);
+            char str[10];
+            sprintf(str, "%lx", value);
+            printf("The value read out is %s\n",str);
             addr[8] = '0';
             msg[8] ='0';
             hex_addr = 0;
